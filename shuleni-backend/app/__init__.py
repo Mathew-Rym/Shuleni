b1be147eee4df4da3a78df3b582478ad4be5757a
Sharon Kendi <sharonkendi35@gmail.com>
2025-07-18 05:26:15 +0300

    Handles database uri

--- shuleni-backend/app/__init__.py
+++ shuleni-backend/app/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     app = Flask(__name__)
 
      
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 
