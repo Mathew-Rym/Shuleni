f070a5b3615e6029403c88a08eb2e57ac149b50b
Neville Muli <neville.muli@gmail.com>
2025-07-22 19:28:39 +0300

    imports bootstrap to main.jsx and creates base header layout

--- shuleni-frontend/src/components/Header.jsx
+++ shuleni-frontend/src/components/Header.jsx
@@ -1,1 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Navbar, Nav, Badge, Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const isAdmin = true; 
+
+    const handleCloseSidebar = () => setShowSidebar(false);
+    const handleShowSidebar = () => setShowSidebar(true);
+  return (
+    <>
+      <Navbar bg="light" expand="lg" className="border-bottom">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">
+            <h3 className="mb-0">Shuleni</h3>
+          </Navbar.Brand>
+          
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/resources">Resources</Nav.Link>
+            <Nav.Link as={Link} to="/classes">Classes</Nav.Link>
+          </Nav>
+          
+          <div className="d-flex align-items-center">
+            {isAdmin && (
+              <Button variant="outline-primary"  size="sm" className="me-2"
+                onClick={handleShowSidebar}
+              >  Open Sidebar</Button>
+            )}
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
