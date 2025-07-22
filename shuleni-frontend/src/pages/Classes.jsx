c22957c6bb24deef2cfff883f80890919b416c75
Neville Muli <neville.muli@gmail.com>
2025-07-22 19:48:23 +0300

    adds base of classes page

---
+++ shuleni-frontend/src/pages/Classes.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+import LessonCard from '../components/LessonCard';
+import ClassMembers from '../components/ClassMembers';
+import Chat from '../components/Chat';
+
+const Class= () => {
+    return(
+        <div>
+
+        </div>
+
+    )
+}
+
+export default Class;
