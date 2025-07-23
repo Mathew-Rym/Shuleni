b2221fbe8a4a2e192352f3461d13d20b69a232b1
Neville Muli <neville.muli@gmail.com>
2025-07-23 22:31:51 +0300

    adds routes to app.jsx, make css changes and finishes resource details page

--- shuleni-frontend/src/App.jsx
+++ shuleni-frontend/src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Class from './pages/Classes.jsx'
 import ExamTaking from './pages/ExamTaking.jsx'
+import ExamOverview from './pages/ExamOverview.jsx'
+import Resources from './pages/Resources.jsx'
+import ResourceDetail from './pages/ResourceDetail.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
@@ -12,7 +16,10 @@ function App() {
     <main>
       <Routes>
         <Route path="/classes" element={<Class />} />
-        <Route path="/exam/" element={<ExamTaking />} />
+        <Route path="/exam-taking" element={<ExamTaking />} />
+        <Route path="/exam-overview" element={<ExamOverview />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/resource/:id" element={<ResourceDetail />} />
       </Routes>
     </main>
     </Router>
