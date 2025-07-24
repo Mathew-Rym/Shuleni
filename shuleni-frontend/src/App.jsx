aad44d053f98014cb632f97729d6662b7198dcfc
Calvis Onyango <calvisonyango265@gmail.com>
2025-07-24 10:04:01 +0300

    commits

--- shuleni-frontend/src/App.jsx
+++ shuleni-frontend/src/App.jsx
@@ -1,46 +1,34 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import Header from './components/Header.jsx';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
-import './App.css'; // Assuming you have some global styles
+import Class from './pages/Classes.jsx';
+import ExamTaking from './pages/ExamTaking.jsx';
+import ExamOverview from './pages/ExamOverview.jsx';
+import Resources from './pages/Resources.jsx';
+import ResourceDetail from './pages/ResourceDetail.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/classes" element={<Class />} />
+          <Route path="/exam-taking" element={<ExamTaking />} />
+          <Route path="/exam-overview" element={<ExamOverview />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/resource/:id" element={<ResourceDetail />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </Router>
   );
-
-import React from 'react'
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
-import Header from './components/Header.jsx'
-import Class from './pages/Classes.jsx'
-import ExamTaking from './pages/ExamTaking.jsx'
-import ExamOverview from './pages/ExamOverview.jsx'
-import Resources from './pages/Resources.jsx'
-import ResourceDetail from './pages/ResourceDetail.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-
-function App() {
-  return (
-   <Router>
-    <Header />
-    <main>
-      <Routes>
-        <Route path="/classes" element={<Class />} />
-        <Route path="/exam-taking" element={<ExamTaking />} />
-        <Route path="/exam-overview" element={<ExamOverview />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/resource/:id" element={<ResourceDetail />} />
-      </Routes>
-    </main>
-    </Router>
-  )
 }
-
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import './App.css'; // Assuming you have some global styles
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
